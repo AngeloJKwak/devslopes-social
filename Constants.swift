3fe618f56c6608d849db11cc0e8a8318fa3fba2d
AngeloJKwak <AngeloKwak@Angelos-MacBook.local>
2017-10-13 16:32:01 -0400

    Sign in UI custom classes complete

---
+++ Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  devslopes-social
+//
+//  Created by Angelo Kwak on 10/13/17.
+//  Copyright © 2017 Angelo Kwak. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0/255.0
